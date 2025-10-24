a3c7ed0034f39beaa879608b43cad099c96529f8
Virginia Balseiro <info@virginiabalseiro.com>
2025-10-24 16:02:53 +0200

    Turns out it wasn't a good idea to let the schema handle the whitespace

--- src/editor/schema/base.js
+++ src/editor/schema/base.js
@@ -176,10 +176,10 @@ let customNodes = {
     content: "block+",
     group: "block",
     attrs: { originalAttributes: { default: {} } },
-    parseDOM: [{ tag: "dd", preserveWhitespace: "full", getAttrs(node){ return getAttributes(node); }}],
-    toDOM(node) { return ["dd", { ...node.attrs.originalAttributes }, 0]; },
-    // parseDOM: [{ tag: "dd", getAttrs(node){ return getAttributes(node); }}],
+    // parseDOM: [{ tag: "dd", preserveWhitespace: "full", getAttrs(node){ return getAttributes(node); }}],
     // toDOM(node) { return ["dd", { ...node.attrs.originalAttributes }, 0]; },
+    parseDOM: [{ tag: "dd", getAttrs(node){ return getAttributes(node); }}],
+    toDOM(node) { return ["dd", { ...node.attrs.originalAttributes }, 0]; },
   },
   ul: {
     content: "li+",
@@ -199,10 +199,10 @@ let customNodes = {
     content: "block+",
     group: "block",
     attrs: { originalAttributes: { default: {} } },
-    // parseDOM: [{ tag: "li", getAttrs(node){ return getAttributes(node); }}],
-    // toDOM(node) { return ["li", { ...node.attrs.originalAttributes }, 0]; },
-    parseDOM: [{ tag: "li", preserveWhitespace: "full", getAttrs(node){ return getAttributes(node); }}],
+    parseDOM: [{ tag: "li", getAttrs(node){ return getAttributes(node); }}],
     toDOM(node) { return ["li", { ...node.attrs.originalAttributes }, 0]; },
+    // parseDOM: [{ tag: "li", preserveWhitespace: "full", getAttrs(node){ return getAttributes(node); }}],
+    // toDOM(node) { return ["li", { ...node.attrs.originalAttributes }, 0]; },
     defining: true
   },
   pre: {
