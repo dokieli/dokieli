8a5b7c58ec81d5d8ca9e8b3b6dc5d5e8aaf2f789
Sarven Capadisli <info@csarven.ca>
2025-12-17 11:02:12 +0100

    Init Web Extension Manifest v3

--- extension-background.js
+++ extension-background.js
@@ -1,72 +1,70 @@
 const WebExtension = (typeof browser !== 'undefined') ? browser : chrome;
 
-var C = {
-  'tabIds': [],
-  'WebID': null
+let C = {
+  tabIds: [],
+  WebID: null
 };
 
-function injectResources(tabId, files) {
-  var getFileExtension = /(?:\.([^.]+))?$/;
-
-  //helper function that returns appropriate chrome.tabs function to load resource
-  var loadFunctionForExtension = (ext) => {
-    switch(ext) {
-      case 'js' : return WebExtension.tabs.executeScript;
-      case 'css' : return WebExtension.tabs.insertCSS;
-      default: throw new Error('Unsupported resource type')
+async function injectResources(tabId, files) {
+  for (const resource of files) {
+    if (resource.endsWith('.css')) {
+      await WebExtension.scripting.insertCSS({
+        target: { tabId },
+        files: [resource]
+      });
     }
-  };
-
-  return Promise.all(files.map(resource => new Promise((resolve, reject) => {
-    var ext = getFileExtension.exec(resource)[1];
-    var loadFunction = loadFunctionForExtension(ext);
-
-    // loadFunction(C.tabId, {file: resource}).then(() => {
-    loadFunction(tabId, {file: resource}, () => {
-      if (WebExtension.runtime.lastError) {
-        reject(WebExtension.runtime.lastError);
-      }
-      else {
-        resolve();
-      }
-    });
-  })));
+    else if (resource.endsWith('.js')) {
+      await WebExtension.scripting.executeScript({
+        target: { tabId },
+        files: [resource]
+      });
+    }
+    else {
+      throw new Error('Unsupported resource type');
+    }
+  }
 }
 
-function dokieliInit(tab) {
-// console.log(tab);
-
-  injectResources(tab.id, ["media/css/dokieli.css"]).then(() => {
-  }).catch(err => {
-     // console.log('Error occurred: '+err);
-  });
+async function dokieliInit(tab) {
+  try {
+    await injectResources(tab.id, ['media/css/dokieli.css']);
+  }
+  catch (e) {
+    // silent
+  }
 }
 
 function showDocumentMenu(tab) {
-  WebExtension.tabs.sendMessage(tab.id, {action: "dokieli.showDocumentMenu", webid: C.WebID}, function(r){
-    if(C.tabIds.indexOf(tab.id) < 0) {
-      C.tabIds.push(tab.id);
+  WebExtension.tabs.sendMessage(
+    tab.id,
+    { action: 'dokieli.showDocumentMenu', webid: C.WebID },
+    () => {
+      if (!C.tabIds.includes(tab.id)) {
+        C.tabIds.push(tab.id);
+      }
     }
-  });
+  );
 }
 
-WebExtension.browserAction.onClicked.addListener(function(tab){
-  WebExtension.tabs.sendMessage(tab.id, {action: "dokieli.status"},
-    function(response) {
+WebExtension.action.onClicked.addListener(async (tab) => {
+  WebExtension.tabs.sendMessage(
+    tab.id,
+    { action: 'dokieli.status' },
+    (response) => {
       if (response && !response.dokieli) {
         dokieliInit(tab);
       }
       showDocumentMenu(tab);
-    });
+    }
+  );
 });
 
-WebExtension.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-  if (request.property == "webid" && C.WebID) {
-    sendResponse({ "webid": C.WebID });
+WebExtension.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.property === 'webid' && C.WebID) {
+    sendResponse({ webid: C.WebID });
   }
   else {
     sendResponse({});
   }
-
   return true;
 });
