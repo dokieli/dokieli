1755f68a6c1967803bb80672e49bed389cb5ee35
Sarven Capadisli <info@csarven.ca>
2025-12-19 20:46:03 +0100

    WIP Add i18n init and locales
    Co-authored-by: Virginia Balseiro <info@virginiabalseiro.com>

---
+++ src/i18n.js
@@ -0,0 +1,25 @@
+import i18next from 'i18next';
+import resourcesToBackend from 'i18next-resources-to-backend';
+import LanguageDetector from 'i18next-browser-languagedetector';
+
+const fallbackLng = {
+  'de-CH': ['fr', 'it'], //French and Italian are also spoken in Switzerland
+  'zh-Hant': ['zh-Hans', 'en'],
+  'es-UY': ['es'],
+  'default': ['en']
+}
+
+const options = {
+  ns: ['translations'],
+  defaultNS: 'translations',
+  fallbackNS: 'translations',
+  debug: true,
+  fallbackLng,
+}
+
+export function i18nextInit() {
+  return i18next
+    .use(resourcesToBackend((language, namespace) => import(`../locales/${language}/${namespace}.json`)))
+    .use(LanguageDetector)
+    .init(options)
+}
